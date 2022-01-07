2801a3f5525626a3b54e1bd60f5957ab80073455
Mike Huang <wellwind@mail2000.com.tw>
2022-01-07 15:23:50 +0800

    練習動態改變 FormGroup 和 FormArray 的內容

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,21 +1,45 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   form = this.formBuilder.group({
     firstName: this.formBuilder.control('Mike'),
-    lastName: this.formBuilder.control('')
+    lastName: this.formBuilder.control(''),
+    password: this.formBuilder.group({
+      password: this.formBuilder.control(''),
+      repeatPassword: this.formBuilder.control(''),
+    }),
+    interest: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+    ]),
   });
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  get interestControlArray(): FormArray {
+    return this.form.get('interest') as FormArray;
   }
 
+  get interestControls(): FormControl[] {
+    return (this.form.get('interest') as FormArray).controls as FormControl[];
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form.addControl('email', this.formBuilder.control(''));
+  }
+
+  remove(index: number) {
+    this.interestControlArray.removeAt(index);
+  }
+
+  add() {
+    this.interestControlArray.push(this.formBuilder.control(''));
+  }
 }
