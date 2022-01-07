8591dce402b896f19364496038030d484060c664
Mike Huang <wellwind@mail2000.com.tw>
2022-01-07 11:45:59 +0800

    檢查 valueChanges, statusChanges 狀態

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,6 @@
+import { HttpClient } from '@angular/common/http';
+import { filter, switchMap } from 'rxjs/operators';
+import { combineLatest, from, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgModel } from '@angular/forms';
@@ -9,8 +12,9 @@ import { NgModel } from '@angular/forms';
 })
 export class LoginComponent implements OnInit, AfterViewInit {
   @ViewChild('tUsername') username?: NgModel;
+  usernameExist = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private httpClient: HttpClient) {}
 
   ngOnInit(): void {}
 
@@ -19,6 +23,36 @@ export class LoginComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       this.username?.control.setValue('mike@miniasp.com');
     }, 0);
+
+    this.username?.valueChanges?.subscribe((data) => {
+      console.log(`username: ${data}`);
+    });
+
+    this.username?.statusChanges?.subscribe((status) => {
+      console.log(`username status: ${status}`);
+    });
+
+    const checkExist = (email: string) => of(true);
+
+    if (
+      this.username &&
+      this.username.valueChanges &&
+      this.username.statusChanges
+    ) {
+      combineLatest([
+        from(this.username.valueChanges),
+        from(this.username.statusChanges),
+      ])
+      .pipe(
+        filter(([username, status]) => status === 'VALID'),
+        switchMap(([username, status]) => checkExist(username)),
+      )
+      .subscribe(exist => {
+        // console.log(username, status);
+        // this.usernameExist = exist;
+        // this.username?.control.setErrors({ usernameExist: true })
+      });
+    }
   }
 
   login(username: string, password: string) {
