1c03b1425bb621d6162d4a6f3254f382f0d0d2e3
Mike Huang <wellwind@mail2000.com.tw>
2022-01-07 09:38:07 +0800

    處理路由事件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'yahome-ng13-advanced';
+  loading = true;
+
+  constructor(private router: Router) {
+
+    // [1, 2, 3, 4].filter(item => item % 2 === 0); // [2, 4]
+
+    router.events.subscribe(event => {
+      if(event instanceof NavigationStart) {
+        this.loading = true;
+      }
+      if(event instanceof NavigationEnd || event instanceof NavigationError) {
+        this.loading = false;
+      }
+    });
+
+    const navigationStart$ = router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    );
+
+    const navigationEnd$ = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    const navigationError$ = router.events.pipe(
+      filter(event => event instanceof NavigationError)
+    )
+
+    const loading$ = navigationStart$;
+    const loaded$ = combineLatest([navigationEnd$, navigationError$]);
+
+    loading$.subscribe(() => {
+      this.loading = true;
+    });
+
+    loaded$.subscribe(() => {
+      this.loading = false;
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
 }
