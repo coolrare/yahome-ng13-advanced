80ea04e8d36dff500e0a8b4704a4940eeeb85dd2
Mike Huang <wellwind@mail2000.com.tw>
2022-01-07 17:08:51 +0800

    參數化的驗證器

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,28 +1,53 @@
 import { Injectable } from '@angular/core';
-import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidatorFn,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 export function forbiddenNameValidator(control: AbstractControl) {
   const nameRe = /Will/;
   const name = control.value;
   const no = nameRe.test(name);
-  return no ? { 'forbiddenName': true } : null;
-};
+  return no ? { forbiddenName: true } : null;
+}
 
+export function forbiddenNameValidator2(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const name = control.value;
+    const no = nameRe.test(name);
+    return no ? { forbiddenName: true } : null;
+  };
+}
+
+const forbiddenName_Mike = forbiddenNameValidator2(/Mike/);
+const forbiddenName_Admin = forbiddenNameValidator2(/Admin/);
+const forbiddenName_Will = forbiddenNameValidator2(/Will/);
+
+const control = new FormControl('', [
+  forbiddenName_Mike,
+  forbiddenName_Admin,
+  forbiddenName_Will,
+]);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   form = this.formBuilder.group({
     firstName: this.formBuilder.control('Mike', [
       Validators.required,
       Validators.minLength(4),
     ]),
-    lastName: this.formBuilder.control({
-      value: '',
-      disabled: true
-    }, Validators.required),
+    lastName: this.formBuilder.control(
+      {
+        value: '',
+        disabled: true,
+      },
+      Validators.required
+    ),
     password: this.formBuilder.group({
       password: this.formBuilder.control(''),
       repeatPassword: this.formBuilder.control(''),
@@ -34,7 +59,7 @@ export class UserService {
     ]),
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   createUserModel() {
     return this.formBuilder.group({
